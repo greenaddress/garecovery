591984bde1fc55fba6598ee1f2dfe0a344662113
Lawrence Nahum <lawrence@greenaddress.it>
2017-08-22 22:47:40 +0200

    update examples

--- garecovery/examples/example_2of2.sh
+++ garecovery/examples/example_2of2.sh
@@ -1,2 +1,2 @@
 TEST_DATA_DIR=garecovery/tests/test_data/
-garecovery-cli --mnemonic-file=$TEST_DATA_DIR/mnemonic_1.txt 2of2 --nlocktime-file=$TEST_DATA_DIR/compressed_1.zip
+garecovery-cli --mnemonic-file $TEST_DATA_DIR/mnemonic_1.txt 2of2 --nlocktime-file $TEST_DATA_DIR/compressed_1.zip
--- garecovery/examples/example_2of3.sh
+++ garecovery/examples/example_2of3.sh
@@ -1,2 +1,2 @@
 TEST_DATA_DIR=garecovery/tests/test_data/
-garecovery-cli --mnemonic-file=$TEST_DATA_DIR/mnemonic_1.txt --testnet 2of3 --rescan --backup-mnemonic-file=$TEST_DATA_DIR/mnemonic_2.txt
+garecovery-cli --mnemonic-file $TEST_DATA_DIR/mnemonic_1.txt 2of3 --rescan --recovery-mnemonic $TEST_DATA_DIR/mnemonic_2.txt
