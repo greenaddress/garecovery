5fc26a113fbb21b737ebb845d9ef5532a2c9d616
Leonardo Comandini <leo@leocomandini.org>
2018-09-24 08:51:21 +0200

    Fix 2of3 example
    Passing more options makes the example run in a reasonable time.

--- garecovery/examples/example_2of3.sh
+++ garecovery/examples/example_2of3.sh
@@ -1,2 +1,2 @@
 TEST_DATA_DIR=garecovery/tests/test_data/
-garecovery-cli --mnemonic-file $TEST_DATA_DIR/mnemonic_1.txt 2of3 --recovery-mnemonic $TEST_DATA_DIR/mnemonic_2.txt
+garecovery-cli --mnemonic-file $TEST_DATA_DIR/mnemonic_1.txt 2of3 --recovery-mnemonic $TEST_DATA_DIR/mnemonic_2.txt --search-subaccounts=1 --destination-address=mynHfTyTWyGGB76NBFbfUrTnn8YWQkTJVs --key-search-depth=10 --scan-from=1535760000 --rpc-timeout-minutes=3
