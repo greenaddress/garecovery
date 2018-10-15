20b8564d97d876f1701358a5896dee3f7a0801ac
Leonardo Comandini <leo@leocomandini.org>
2018-10-15 17:45:16 +0200

    Update network flag: examples

--- garecovery/examples/2of2_summary.sh
+++ garecovery/examples/2of2_summary.sh
@@ -1,2 +1,2 @@
 TEST_DATA_DIR=garecovery/tests/test_data/
-garecovery-cli --mnemonic-file=$TEST_DATA_DIR/mnemonic_1.txt --show-summary 2of2 --nlocktime-file=$TEST_DATA_DIR/compressed_1.zip
+garecovery-cli --network=testnet --mnemonic-file=$TEST_DATA_DIR/mnemonic_1.txt --show-summary 2of2 --nlocktime-file=$TEST_DATA_DIR/compressed_1.zip
