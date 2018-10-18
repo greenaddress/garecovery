2df8c7fea01428f692400693f99c36627063d1fd
Leonardo Comandini <leo@leocomandini.org>
2018-10-18 17:42:58 +0200

    Move mnemonic functions to a separate file

--- garecovery/recoverycli.py
+++ garecovery/recoverycli.py
@@ -7,6 +7,7 @@ import wallycore as wally
 from . import clargs
 from . import exceptions
 from . import formatting
+from .mnemonic import _decrypt_mnemonic, seed_from_mnemonic, wallet_from_mnemonic
 
 from .two_of_two import TwoOfTwo
 from .two_of_three import TwoOfThree
@@ -17,50 +18,6 @@ try:
     user_input = raw_input
 except NameError:
     user_input = input
-
-
-def seed_from_mnemonic(mnemonic_or_hex_seed):
-    """Return seed, mnemonic given an input string
-
-    mnemonic_or_hex_seed can either be:
-    - A mnemonic
-    - A hex seed, with an 'X' at the end, which needs to be stripped
-
-    seed will always be returned, mnemonic may be None if a seed was passed
-    """
-    if mnemonic_or_hex_seed.endswith('X'):
-        mnemonic = None
-        seed = wally.hex_to_bytes(mnemonic_or_hex_seed[:-1])
-    else:
-        mnemonic = mnemonic_or_hex_seed
-        written, seed = wally.bip39_mnemonic_to_seed512(mnemonic_or_hex_seed, None)
-        assert written == wally.BIP39_SEED_LEN_512
-
-    assert len(seed) == wally.BIP39_SEED_LEN_512
-    return seed, mnemonic
-
-
-def wallet_from_mnemonic(mnemonic_or_hex_seed, ver=wally.BIP32_VER_MAIN_PRIVATE):
-    """Generate a BIP32 HD Master Key (wallet) from a mnemonic phrase or a hex seed"""
-    seed, mnemonic = seed_from_mnemonic(mnemonic_or_hex_seed)
-    return wally.bip32_key_from_seed(seed, ver, wally.BIP32_FLAG_SKIP_HASH)
-
-
-def _decrypt_mnemonic(mnemonic, password):
-    """Decrypt a 27 word encrypted mnemonic to a 24 word mnemonic"""
-    mnemonic = ' '.join(mnemonic.split())
-    entropy = bytearray(wally.BIP39_ENTROPY_LEN_288)
-    assert wally.bip39_mnemonic_to_bytes(None, mnemonic, entropy) == len(entropy)
-    salt, encrypted = entropy[32:], entropy[:32]
-    derived = bytearray(64)
-    wally.scrypt(password.encode('utf-8'), salt, 16384, 8, 8, derived)
-    key, decrypted = derived[32:], bytearray(32)
-    wally.aes(key, encrypted, wally.AES_FLAG_DECRYPT, decrypted)
-    for i in range(len(decrypted)):
-        decrypted[i] ^= derived[i]
-    if wally.sha256d(decrypted)[:4] != salt:
-        raise exceptions.InvalidMnemonicOrPasswordError('Incorrect password')
-    return wally.bip39_mnemonic_from_bytes(None, decrypted)
 
 
 def get_mnemonic(args, attr='mnemonic_file', prompt='mnemonic/hex seed: '):
