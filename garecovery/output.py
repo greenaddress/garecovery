31202095289bef246639f9de2744a8643c925ec3
Leonardo Comandini <leo@leocomandini.org>
2019-10-18 15:27:58 +0200

    Liquid: add output module

---
+++ garecovery/output.py
@@ -0,0 +1,78 @@
+import wallycore as wally
+from gaservices.utils.gaconstants import get_address_versions, CA_PREFIX
+
+
+class GreenOutput(object):
+    """Base class for Green outputs"""
+
+    def __init__(self, key, service_pubkey, network='mainnet'):
+        self.key = key  # ECKey
+        self.service_pubkey = service_pubkey  # bytes
+        self.network = network
+
+
+class Green2of2CSVOutput(GreenOutput):
+    """P2SH-P2WSH-2of2-CSV Green output"""
+
+    def __init__(self, key, service_pubkey, csv_blocks, network='mainnet'):
+        super(Green2of2CSVOutput, self).__init__(key, service_pubkey, network)
+        self.csv_blocks = csv_blocks
+
+    @property
+    def witness_script(self):
+        keys = self.service_pubkey + self.key.pub
+        return wally.scriptpubkey_csv_2of2_then_1_from_bytes(keys, self.csv_blocks, 0)
+
+    @property
+    def witness_program(self):
+        return wally.sha256(self.witness_script)
+
+    @property
+    def redeem_script(self):
+        return wally.witness_program_from_bytes(self.witness_script, wally.WALLY_SCRIPT_SHA256)
+
+    @property
+    def script_pubkey(self):
+        return wally.scriptpubkey_p2sh_from_bytes(self.redeem_script, wally.WALLY_SCRIPT_HASH160)
+
+    @property
+    def address(self):
+        script_hash = wally.hash160(self.redeem_script)
+        # FIXME: be more explicit
+        version = bytearray([get_address_versions(self.network)[1]])
+        return wally.base58check_from_bytes(version + script_hash)
+
+    def sign(self, h):
+        """Produce a DER encoded signature using the user key"""
+        return self.key.sign(h)
+
+    @property
+    def script_sig(self):
+        return wally.script_push_from_bytes(self.redeem_script, 0)
+
+    def get_signed_witness(self, h, sighash=wally.WALLY_SIGHASH_ALL):
+        """Produce witness stack assuming CSV time is expired"""
+        witness = wally.tx_witness_stack_init(1)
+        wally.tx_witness_stack_add(witness, self.sign(h) + bytes([sighash]))
+        wally.tx_witness_stack_add(witness, self.witness_script)
+        return witness
+
+
+class Green2of2CSVElementsOutput(Green2of2CSVOutput):
+    """P2SH-P2WSH-2of2-CSV Green Elements output"""
+
+    # FIXME: consider adding a seed/master_blinding_key param
+    def __init__(self, key, service_pubkey, csv_blocks, network='liquid'):
+        super(Green2of2CSVElementsOutput, self).__init__(key, service_pubkey, csv_blocks, network)
+
+    def get_private_blinding_key(self, seed):
+        master_blinding_key = wally.asset_blinding_key_from_seed(seed)
+        return wally.asset_blinding_key_to_ec_private_key(master_blinding_key, self.script_pubkey)
+
+    def get_public_blinding_key(self, seed):
+        return wally.ec_public_key_from_private_key(self.get_private_blinding_key(seed))
+
+    def get_confidential_address(self, seed):
+        ca_prefix = CA_PREFIX[self.network]
+        return wally.confidential_addr_from_addr(
+            self.address, ca_prefix, self.get_public_blinding_key(seed))
--- gaservices/utils/gaconstants.py
+++ gaservices/utils/gaconstants.py
@@ -1,6 +1,7 @@
 """ Constant values for recovery/BTC """
 import decimal
 import sys
+import wallycore as wally
 
 PY3 = sys.version_info.major > 2
 
@@ -11,7 +12,7 @@ MAX_BIP125_RBF_SEQUENCE = 0xfffffffd
 # BIP32 hardened derivation flag
 HARDENED = 0x80000000
 
-SUPPORTED_NETWORKS = ['mainnet', 'testnet']
+SUPPORTED_NETWORKS = ['mainnet', 'testnet', 'liquid', 'localtest-liquid']
 
 P2PKH_MAINNET = 0x00
 P2SH_MAINNET = 0x05
@@ -19,17 +20,57 @@ P2SH_MAINNET = 0x05
 P2PKH_TESTNET = 0x6f
 P2SH_TESTNET = 0xc4
 
+P2PKH_LIQUID = 0x39
+P2SH_LIQUID = 0x27
+
+P2PKH_LIQUID_REGTEST = 0xeb
+P2SH_LIQUID_REGTEST = 0x4b
+
 ADDR_VERSIONS_MAINNET = [P2PKH_MAINNET, P2SH_MAINNET]
 ADDR_VERSIONS_TESTNET = [P2PKH_TESTNET, P2SH_TESTNET]
+ADDR_VERSIONS_LIQUID = [P2PKH_LIQUID, P2SH_LIQUID]
+ADDR_VERSIONS_LIQUID_REGTEST = [P2PKH_LIQUID_REGTEST, P2SH_LIQUID_REGTEST]
 
 ADDR_FAMILY_MAINNET = 'bc'
 ADDR_FAMILY_TESTNET = 'tb'
+ADDR_FAMILY_LIQUID = 'lq'
+ADDR_FAMILY_LIQUID_REGTEST = 'tb'
+
+CA_PREFIX = {
+    'liquid': wally.WALLY_CA_PREFIX_LIQUID,
+    'localtest-liquid': wally.WALLY_CA_PREFIX_LIQUID_REGTEST,
+}
+
+CSV_BUCKETS = {
+    'liquid': [65535],
+    'localtest-liquid': [144, 4320, 25920, 51840, 65535],
+    'testnet': [144, 4320, 51840],
+    'mainnet': [25920, 51840, 65535],
+}
+
+# TODO: correctly estimate transaction vsize
+# The following values overestimate the impact of adding an input or output to the transaction vsize, in order to
+# produce a valid feerate. A temporary solution can be tweaking such values in order to match the desired feerate.
+LIQUID_EMPTY_TX_SIZE = 40
+LIQUID_INPUT_SIZE = 150
+LIQUID_OUTPUT_SIZE = 1200
+
 
 def get_address_versions(network):
-    return {'testnet': ADDR_VERSIONS_TESTNET, 'mainnet': ADDR_VERSIONS_MAINNET}[network]
+    return {
+        'testnet': ADDR_VERSIONS_TESTNET,
+        'mainnet': ADDR_VERSIONS_MAINNET,
+        'liquid': ADDR_VERSIONS_LIQUID,
+        'localtest-liquid': ADDR_VERSIONS_LIQUID_REGTEST,
+    }[network]
 
 def get_address_family(network):
-    return {'testnet': ADDR_FAMILY_TESTNET, 'mainnet': ADDR_FAMILY_MAINNET}[network]
+    return {
+        'testnet': ADDR_FAMILY_TESTNET,
+        'mainnet': ADDR_FAMILY_MAINNET,
+        'liquid': ADDR_FAMILY_LIQUID,
+        'localtest-liquid': ADDR_FAMILY_LIQUID_REGTEST,
+    }[network]
 
 # GreenAddress script type for standard p2sh multisig UTXOs
 P2SH_FORTIFIED_OUT = 10
@@ -47,5 +88,17 @@ GA_KEY_DATA_TESTNET = {
     'pubkey': '036307e560072ed6ce0aa5465534fb5c258a2ccfbc257f369e8e7a181b16d897b3',
 }
 
+GA_KEY_DATA_LIQUID = {
+    'chaincode': '02721cc509aa0c2f4a90628e9da0391b196abeabc6393ed4789dd6222c43c489',
+    'pubkey': '02c408c3bb8a3d526103fb93246f54897bdd997904d3e18295b49a26965cb41b7f'
+}
+
+GA_KEY_DATA_LIQUID_REGTEST = GA_KEY_DATA_TESTNET
+
 def get_ga_key_data(network):
-    return {'testnet': GA_KEY_DATA_TESTNET, 'mainnet': GA_KEY_DATA_MAINNET}[network]
+    return {
+        'testnet': GA_KEY_DATA_TESTNET,
+        'mainnet': GA_KEY_DATA_MAINNET,
+        'liquid': GA_KEY_DATA_LIQUID,
+        'localtest-liquid': GA_KEY_DATA_LIQUID_REGTEST,
+    }[network]
